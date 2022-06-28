d94beb421ffbafbb5be1a5f4e7a414fae85b2bf9
milutinstanojevic <milutinstan@outlook.fr>
2022-06-28 14:03:50 +0200

    initial test

---
+++ server/src/main.rs
@@ -0,0 +1,26 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::Read;
+
+fn main() {
+
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!(
+        "Test: {}",
+        String::from_utf8_lossy(&buffer[135..])
+    );
+}
